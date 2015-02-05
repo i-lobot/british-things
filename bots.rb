47d093b1d38085421a8f4108f578fa2aeac43cca
Jon Williams <jonathan.williams@gmail.com>
2015-02-05 01:19:29 -0500

    british bot

--- bots.rb
+++ bots.rb
@@ -8,8 +8,9 @@ class MyBot < Ebooks::Bot
   def configure
     # Consumer details come from registering an app at https://dev.twitter.com/
     # Once you have consumer details, use "ebooks auth" for new access tokens
-    self.consumer_key = '' # Your app consumer key
-    self.consumer_secret = '' # Your app consumer secret
+    config = YAML.load_file('secrets.yml')
+    self.consumer_key = config['consumer_key']
+    self.consumer_secret = config['consumer_secret']
 
     # Users to block instead of interacting with
     self.blacklist = ['tnietzschequote']
@@ -19,37 +20,26 @@ class MyBot < Ebooks::Bot
   end
 
   def on_startup
-    scheduler.every '24h' do
+    @logic = BritainLogic.new
+    generate()
+    scheduler.every '1h' do
       # Tweet something every 24 hours
       # See https://github.com/jmettraux/rufus-scheduler
       # tweet("hi")
       # pictweet("hi", "cuteselfie.jpg")
+      generate()
     end
   end
 
-  def on_message(dm)
-    # Reply to a DM
-    # reply(dm, "secret secrets")
+  def generate()
+    tweet(@logic.generate)
   end
 
-  def on_follow(user)
-    # Follow a user back
-    # follow(user.screen_name)
-  end
-
-  def on_mention(tweet)
-    # Reply to a mention
-    # reply(tweet, "oh hullo")
-  end
-
-  def on_timeline(tweet)
-    # Reply to a tweet in the bot's timeline
-    # reply(tweet, "nice tweet")
-  end
 end
 
 # Make a MyBot and attach it to an account
 MyBot.new("british-updates") do |bot|
-  bot.access_token = "" # Token connecting the app to this account
-  bot.access_token_secret = "" # Secret connecting the app to this account
+  config = YAML.load_file('secrets.yml')
+  bot.access_token = config['access_token']
+  bot.access_token_secret = config['access_token_secret']
 end
